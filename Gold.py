e2a80cd47977b06e10c6fb61b6446b66e6766600
Mylo Cyrus <irahiggins740@gmail.com>
2023-06-29 17:43:50 -0700

    Add files via upload

---
+++ Gold.py
@@ -0,0 +1,18 @@
+
+
+```python
+print("Golden Madonna")
+```
+
+This program will output:
+
+```
+Golden Madonna
+
+
+Certainly! Here's a simple Python program that prints "Gold" on the console:
+
+```python
+print("Gold")
+```
+
